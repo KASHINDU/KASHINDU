5d58b6636eaef89f8083ea9315e99b7514de8e1a
KASHINDU <kashindukrushiunnatifpc@gmail.com>
2025-07-13 16:48:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold">Welcome to Kashindu Krushi Unnati FPC</h1>
+      <p className="mt-4">This is the bilingual React + Tailwind website.</p>
+    </div>
+  );
+}
+
+export default App;
